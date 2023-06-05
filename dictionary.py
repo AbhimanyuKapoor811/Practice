1e9a9924f8fab8071478c8447ecec886cfcb2f42
AbhimanyuKapoor811 <135612554+AbhimanyuKapoor811@users.noreply.github.com>
2023-06-05 18:40:32 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,38 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+}
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+empty_dictionary = {}
+programming_dictionary["Bug"] = "A moth in your computer."
+
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+
+travel_log = [
+{
+  "country": "France", 
+  "cities_visited": ["Paris", "Lille", "Dijon"], 
+  "total_visits": 12,
+},
+{
+  "country": "Germany",
+  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+  "total_visits": 5,
+},
+]
